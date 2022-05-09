a846943638a049778cebf6b2514ff436708fe003
Danni Kline <danniellekline@gmail.com>
2022-05-09 15:22:29 -0400

    Updating audio capture files

---
+++ src/main/java/edu/oswego/cs/AudioTester.java
@@ -0,0 +1,35 @@
+package edu.oswego.cs;
+
+import javax.swing.plaf.TableHeaderUI;
+import java.io.IOException;
+import java.net.Socket;
+
+public class AudioTester {
+
+    public static void main (String[] args) throws IOException {
+        final AudioCapture ac = new AudioCapture();
+
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted Exception");
+                    e.printStackTrace();
+                }
+                ac.stopCapture();
+
+                //send recorded file
+                try {
+                    ac.sendWavOverTCP(new Socket("localhost", 2700));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        stopper.start();
+
+        //start recording
+        ac.startCapture();
+    }
+}
